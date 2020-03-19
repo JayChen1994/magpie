3b29df5b36be2075cef115710e6c0706de5e4358
pakuwa <shylunule@gmail.com>
2020-03-20 01:12:22 +0800

    fix

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -31,11 +31,53 @@ class IndexController extends Controller
 
     public function authorizeUser(Request $request)
     {
-        Log::info("arguments", $request->all());
+        Log::info("retrieve code...", $request->all());
+
+        $code = $request->input('code');
+        $state = $request->input('state');
+        $appId = env('WX_APPID');
+        $appSecret = env('WX_SECRET');
+
+        $ret = $this->getWebPage("https://api.weixin.qq.com/sns/oauth2/access_token?appid=$appId&secret=$appSecret&code=$code&grant_type=authorization_code");
+        Log::info("retrieve access_token...", $ret);
+
+        $accessToken = $ret['access_token'];
+        $openId = $ret['openid'];
+
+        $useInfo = $this->getWebPage("https://api.weixin.qq.com/sns/userinfo?access_token=$accessToken&openid=$openId&lang=zh_CN");
+        Log::info("userInfo...", $useInfo);
+
+        redirect(env('APP_URL'));
     }
 
     private function getImgUrl($file)
     {
         return env('APP_URL') . '/imgs/' . $file;
+    }
+
+    private function getWebPage($url)
+    {
+        $options = array(
+            CURLOPT_RETURNTRANSFER => true,     // return web page
+            CURLOPT_HEADER => false,            // don't return headers
+            CURLOPT_FOLLOWLOCATION => true,     // follow redirects
+            CURLOPT_ENCODING => "",             // handle all encodings
+            CURLOPT_USERAGENT => "magpie",      // who am i
+            CURLOPT_AUTOREFERER => true,        // set referer on redirect
+            CURLOPT_CONNECTTIMEOUT => 120,      // timeout on connect
+            CURLOPT_TIMEOUT => 120,             // timeout on response
+            CURLOPT_MAXREDIRS => 10,            // stop after 10 redirects
+            CURLOPT_SSL_VERIFYPEER => false     // Disabled SSL Cert checks
+        );
+
+        $ch = curl_init($url);
+        curl_setopt_array($ch, $options);
+        $content = curl_exec($ch);
+        curl_close($ch);
+
+        // $header['errno'] = $err;
+        // $header['errmsg'] = $errmsg;
+        // $header['content'] = $content;
+        return json_decode($content, true);
     }
 }
