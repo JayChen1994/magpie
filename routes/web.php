4c8dd74da961e716ecd8d33176e4e8181d166b6d
JayChen1994 <1102324268@qq.com>
2020-03-28 17:01:15 +0800

    fix

--- routes/web.php
+++ routes/web.php
@@ -28,12 +28,12 @@ $router->get('/api/user/jspackage', [['middleware' => 'auth'], 'uses' => 'IndexC
 $router->get('/api/user/islogin', [['middleware' => 'auth'], 'uses' => 'IndexController@isLogin']);
 
 $router->group(['middleware' => ['web', 'auth']], function () use ($router) {
-    $router->get('/order/get-detail', ['uses' => 'OrderController@getOrderDetail']); // 订单详情
-    $router->post('/pay/toPay', ['uses' => 'PayController@toPay']); // 去支付
-    $router->get('/order/list', ['uses' => 'OrderController@list']); // 订单列表
-    $router->get('/order/use-list', ['uses' => 'OrderController@useList']); // 历史记录
-    $router->get('/order/to-use', ['uses' => 'OrderController@toUse']); // 同使用
-    $router->get('/order/admin-use-log', ['uses' => 'OrderController@adminUseLog']); // 同使用
+    $router->get('/api/order/get-detail', ['uses' => 'OrderController@getOrderDetail']); // 订单详情
+    $router->post('/api/pay/toPay', ['uses' => 'PayController@toPay']); // 去支付
+    $router->get('/api/order/list', ['uses' => 'OrderController@list']); // 订单列表
+    $router->get('/api/order/use-list', ['uses' => 'OrderController@useList']); // 历史记录
+    $router->get('/api/order/to-use', ['uses' => 'OrderController@toUse']); // 同使用
+    $router->get('/api/order/admin-use-log', ['uses' => 'OrderController@adminUseLog']); // 同使用
 });
 
-$router->post('/pay/notify', ['uses' => 'PayController@notify']); // 微信回调
+$router->post('/api/pay/notify', ['uses' => 'PayController@notify']); // 微信回调
